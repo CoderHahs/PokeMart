09955255692fde21c9e7dd2b061b172f82a90da6
Hrithik Shah <hrithikchamp@gmail.com>
2020-12-23 21:30:33 -0500

    user screen

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,17 +1,65 @@
 import React from "react";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
+import Icon from "../components/Icon";
+import { FlatList, View, StyleSheet } from "react-native";
+import colors from "../config/colors";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
 
 function AccountScreen(props) {
   return (
-    <Screen>
-      <ListItem
-        title="Hrithik Shah"
-        subTitle="hrithikhahs@gmail.com"
-        image={require("../assets/hrithik.jpg")}
-      />
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title="Hrithik Shah"
+          subTitle="hrithikhahs@gmail.com"
+          image={require("../assets/hrithik.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(item) => item.title}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              ImageComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+  screen: {
+    backgroundColor: colors.lightGrey,
+  },
+});
 
 export default AccountScreen;
